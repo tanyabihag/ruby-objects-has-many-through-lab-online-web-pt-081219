905c7533150bd797742ebb787ea814cdd9313da7
Tanya Bihag <tbihag@gmail.com>
2019-10-06 01:37:47 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+ class Patient
+
+@@all = []
+attr_accessor :name 
+
+def initialize(name)
+  @name = name
+  @@all << self 
+end 
+
+ def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+    
+  end
+  
+def self.all 
+  @@all
+end 
+
+def appointments
+  Appointment.all.select {|item| item.patient == self}
+end 
+
+  def doctors
+    doctor_list = []
+    self.appointments.each {|item| doctor_list << item.doctor}
+    doctor_list
+  end
+end
