994fda0a495e6ed7be73012b78d7bafd8b0ac3d3
Tanya Bihag <tbihag@gmail.com>
2019-10-06 00:09:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ end
 
 def patients
   patient_list = []
-  appointments.each {|item| patient_list << item.patient}
+  self.appointments.each {|item| patient_list << item.patient}
   patient_list
 end
 
