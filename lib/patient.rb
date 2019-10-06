d89ae889faf74f5c1f7fe8af2ce4b864ebb12ea8
Tanya Bihag <tbihag@gmail.com>
2019-10-06 01:36:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,31 +1,28 @@
-class Patient 
+ class Patient
 
-  attr_accessor :name 
+@@all = []
+attr_accessor :name 
 
-  @@all = [ ]
-
-  def initialize 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(doctor, date)
-    Appointment.new(date, self, doctor)
-  
-  end
-
-  def appointments
-    Appointment.all.select {|item| item.patient == self }
-  end
-
-  def doctors
-    doctor_list = []
-    self.appointments.each {|item| doctor_list << item.doctor}
-    doctor_list
-  end
+def initialize(name)
+  @name = name
+  @@all << self 
 end 
+
+def new_appointment(doctor, date)
+  Appointment.new(doctor, date, self)
+end 
+
+def self.all 
+  @@all
+end 
+
+def appointments
+  Appointment.all.select {|item| item.patient == self}
+end 
+
+def doctors
+  doctor_list = []
+  self.appointments.each {|item| doctor_list << item.doctor}
+  doctor_list
+end
+end
